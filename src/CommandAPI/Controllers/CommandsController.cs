34655bb240d08c3dc9ef04afd47018e0cf2464cf
Irina <balkarovairina@gmail.com>
2021-08-06 10:02:21 +0200

    add: CommandController

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Index()
+        {
+            return new string[] { "this", "is", "hard", "codes" }; 
+        }
+    }
+}
